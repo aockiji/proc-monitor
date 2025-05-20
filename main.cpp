d1b0f37d3f5ade4d2e103793850fbdc71aaffe5c
aockiji <favourackonu13@gmail.com>
2025-05-20 17:20:33 +0000

    added function to list open files per process

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <dirent.h>
 #include <algorithm>
 #include <cctype>
+#include <filesystem>
 
 using namespace std;
 
@@ -30,11 +31,43 @@ vector<int> getProcessIDs(const string& dirName = "/proc"){
     return PIDs;
 }
 
+void printSymlinkTargets(const string& fdPath){
+    DIR *dir = opendir(fdPath.c_str());
+
+    if(!dir){
+        cout << "None" << endl;
+        return;
+    }
+
+    struct dirent *entry;
+
+    try{
+        while((entry = readdir(dir)) != nullptr){
+            if(entry -> d_name[0] == '.'){
+                continue;
+            }
+            string symlinkPath = string(fdPath) + "/" + string(entry -> d_name);
+        
+            if(filesystem::is_symlink(symlinkPath)){
+                filesystem::path target = filesystem::read_symlink(symlinkPath); 
+                cout << symlinkPath << " -> " << target << endl;
+            }  
+        }
+    }
+    catch(const exception& e){
+        cerr << "Exception: " << e.what() << endl;
+    }
+}
+
 int main(){
-    vector<int> PIDs = getProcessIDs();
+    string dir = "/proc";
+    vector<int> PIDs = getProcessIDs(dir);
 
     for(int PID : PIDs){
-        cout << PID << endl;
+        cout << "PROCESS ID: " << PID << endl;
+        string fdPath = (dir + "/" + to_string(PID) + "/fd").c_str();
+        printSymlinkTargets(fdPath);
+        cout << endl;
     }
 
     return 0;
